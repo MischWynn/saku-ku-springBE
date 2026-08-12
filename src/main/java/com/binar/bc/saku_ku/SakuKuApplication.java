6ffdb71f716e763663fabda4ae43760f2d34ef23
unknown <lauravlaiina@gmail.com>
2026-08-12 16:53:24 +0700

    initial commit checkin project

---
+++ src/main/java/com/binar/bc/saku_ku/SakuKuApplication.java
@@ -0,0 +1,13 @@
+package com.binar.bc.saku_ku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakuKuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakuKuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binar/bc/saku_ku/SakuKuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binar.bc.saku_ku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakuKuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
